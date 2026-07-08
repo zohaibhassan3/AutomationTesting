f9318f2a252f843a8a9b8f81590624ffb1d38b27
zohaibhassan3 <zohaibenroute3@gmail.com>
2026-07-08 11:49:26 +0500

    Update e2e.js

--- cypress/support/e2e.js
+++ cypress/support/e2e.js
@@ -15,3 +15,23 @@
 
 // Import commands.js using ES2015 syntax:
 import './commands'
+// 1. Hide fetch/XHR requests from command log (cleaner logs)
+const app = window.top
+if (!app.document.head.querySelector('[data-hide-command-log-request]')) {
+  const style = app.document.createElement('style')
+  style.innerHTML = '.command-name-request, .command-name-xhr { display: none }'
+  style.setAttribute('data-hide-command-log-request', '')
+  app.document.head.appendChild(style)
+}
+
+
+
+// 2. Ignore uncaught exceptions
+Cypress.on('uncaught:exception', (err, runnable) => {
+  return false
+})
+
+// 3. Print test name in console
+Cypress.on('test:before:run', (test) => {
+  console.log('Running test:', test.title)
+})
